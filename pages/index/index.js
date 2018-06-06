48bbec1f4567866fe1ddad9eaf206637e29615ee
cdasl <287271860@qq.com>
2018-06-06 19:03:32 +0800

    2nd commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,80 +1,20 @@
-// //index.js
-// //获取应用实例
-// // const app = getApp()
-// var util = require("../../utils/util.js")
-// Page({
-//   data: {
-//     weather: {}
-//     // motto: 'Hello World',
-//     // userInfo: {},
-//     // hasUserInfo: false,
-//     // canIUse: wx.canIUse('button.open-type.getUserInfo')
-//   },
-//   //事件处理函数
-//   // bindViewTap: function() {
-//   //   wx.navigateTo({
-//   //     url: '../logs/logs'
-//   //   })
-//   // },
-//   onLoad: function () {
-//     var that = this
-//     util.loadWeatherData(function(data){
-//       that.setData({
-//         weather: data
-//       })
-//     })
-//     // if (app.globalData.userInfo) {
-//     //   this.setData({
-//     //     userInfo: app.globalData.userInfo,
-//     //     hasUserInfo: true
-//     //   })
-//     // } else if (this.data.canIUse){
-//     //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-//     //   // 所以此处加入 callback 以防止这种情况
-//     //   app.userInfoReadyCallback = res => {
-//     //     this.setData({
-//     //       userInfo: res.userInfo,
-//     //       hasUserInfo: true
-//     //     })
-//     //   }
-//     // } else {
-//     //   // 在没有 open-type=getUserInfo 版本的兼容处理
-//     //   wx.getUserInfo({
-//     //     success: res => {
-//     //       app.globalData.userInfo = res.userInfo
-//     //       this.setData({
-//     //         userInfo: res.userInfo,
-//     //         hasUserInfo: true
-//     //       })
-//     //     }
-//     //   })
-//     // }
-//   },
-//   getUserInfo: function(e) {
-//     console.log(e)
-//     app.globalData.userInfo = e.detail.userInfo
-//     this.setData({
-//       userInfo: e.detail.userInfo,
-//       hasUserInfo: true
-//     })
-//   }
-// })
-//index.js
-//获取应用实例
-
 var util = require('../../utils/util.js')
 
 Page({
 
   data: {
-    weather: {}
+    weather: {}, 
+    bgColor: ""
   },
   onShow: function () {
     wx.showLoading({
       title: '请稍等',
     })
-    var that = this;
 
+    var that = this;
+    that.setData({
+      bgColor: "../../images/bg.jpg"
+    })
     util.loadWeatherData(function (data) {
 
       console.log(data);
@@ -93,5 +33,6 @@ Page({
       url: '../citySelect/cs'
     })
   }
+  
 
 })
